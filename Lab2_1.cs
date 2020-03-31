ae126a5320897eaaf114900fa832e34073295795
Andrew <44731353+AVPa1ly@users.noreply.github.com>
2020-03-31 22:54:41 +0300

    Add files via upload

---
+++ Lab2_1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+using System.Globalization;
+
+namespace Lab2_1
+{
+    //task 1 from tasklist
+    class Lab2_1
+    {
+        static void Main(string[] args)
+        {
+            DateTime currentDate = DateTime.Now;
+            StringBuilder strDate = new StringBuilder(currentDate.ToString("MMddyyyyHHmmss"));
+            byte[] nums = new byte[10];
+            for (int i = 0; i < strDate.Length; i++)
+            {
+                nums[strDate[i] - 48]++;
+            }
+            Console.WriteLine("Full date-time: {0}", currentDate.ToString("F", CultureInfo.CreateSpecificCulture("en-US")));
+            Console.WriteLine("Shortened date-time: {0:G}\n", currentDate);
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i]!= 0)
+                {
+                    Console.WriteLine($"Number of {i}s in current date = {nums[i]}");
+                }
+            }
+            Console.WriteLine("\nPress any key to proceed...");
+            Console.ReadKey();            
+        }
+    }
+}
---
+++ Lab2_2.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Text;
+using System.IO;
+
+namespace Lab2_2
+{
+    //task 13 from tasklist
+    class Lab2_2
+    {
+        static void Main(string[] args)
+        {
+            const byte numofSymbols = 30;
+            const int numofLetters = 256; 
+            StringBuilder letters;
+            while (true)
+            {
+                Console.WriteLine("Enter a string, that consists of 256 english letters:");
+                Console.SetIn(new StreamReader(Console.OpenStandardInput(8192)));
+                letters = new StringBuilder(Console.ReadLine());
+                if (letters.Length!= numofLetters)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Incorrect input, repeat");
+                    continue;
+                }
+
+                int i = 0;
+                for (; i < numofLetters; i++)
+                {
+                    if (!char.IsLetter(letters[i]))
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Incorrect input, repeat");
+                        break;
+                    }
+                }
+
+                if (i != numofLetters)
+                    continue;
+
+                break;
+            }
+
+            byte[] sequence = new byte[numofSymbols];
+            Random random = new Random();
+            random.NextBytes(sequence);
+            Console.WriteLine();
+            for (int i = 0; i < numofSymbols; i++)
+            {
+                Console.Write("{0} ", letters[sequence[i]]);
+            }
+            Console.WriteLine("\nPress any key to proceed...");
+            Console.ReadKey();
+        }
+    }
+}
