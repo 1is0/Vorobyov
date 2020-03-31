1eba426edf49e16ad5081f9eac029e0f20efab9d
Andrew <44731353+AVPa1ly@users.noreply.github.com>
2020-03-31 22:55:35 +0300

    Add files via upload

---
+++ lab1/Lab2_3.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lab2_3
+{
+    //task 11 from tasklist
+    class Lab2_3
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Enter your string:");
+            string str = Console.ReadLine();
+            string[] words = str.Split(new char[] { ' ' });
+            Console.WriteLine();
+            foreach (string word in words)
+            {
+                if (word.IndexOf("...") != -1)
+                {
+                    Console.Write("...");
+                }
+                else if (word[word.Length - 1] == ';' || word[word.Length - 1] == ',' || word[word.Length - 1] == '.' || word[word.Length - 1] == '!' || word[word.Length - 1] == '?' || word[word.Length - 1] == ':' || word[word.Length - 1] == '\"' || word[word.Length - 1] == '\'')
+                {
+                    Console.Write(word[word.Length - 1]);
+                }
+                Console.Write(word + " ");
+            }
+            Console.WriteLine("\nPress any key to proceed...");
+            Console.ReadKey();
+        }
+    }
+}
