0496df6190347093d71849e464dc2cab24b6fead
AVPa1ly <AVPa1ly@users.noreply.github.com>
2020-04-29 18:50:04 +0300

    Added lab 7
    See comments in code

---
+++ C#/lab7/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+
+namespace Numbers
+{
+    class Program
+    {
+        delegate void ExceptionHandler();
+        static void Main(string[] args)
+        {
+            Rationals[] rats = new Rationals[6];
+            rats[0] = new Rationals();
+            rats[1] = new Rationals(12, 13);
+            rats[2] = "12,1345";
+            rats[3] = "12.1345";
+            rats[4] = 5555.5555;
+            rats[5] = -999.99999;
+            FormattingDemo(rats);
+            ComparisonsDemo(rats);
+            OperationsDemo(rats);
+            MistakesDemo(rats);
+            Console.ReadKey();
+        }
+        static void ColoredWriteLine(ConsoleColor clr, string msg)
+        {
+            Console.ForegroundColor = clr;
+            Console.WriteLine(msg);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        static void FormattingDemo(Rationals[] rats)
+        {
+            ColoredWriteLine(ConsoleColor.Green, $"{"ToString() method (or DoubleView()):\n",76}");
+            for (int i = 0; i < rats.Length; i++)
+            {
+                Console.WriteLine("rationalsArray[{0}] = {1}", i, rats[i].ToString());
+            }
+            ColoredWriteLine(ConsoleColor.DarkGreen, "\n" + $"{"Scientific format:\n",65}");
+            for (int i = 0; i < rats.Length; i++)
+            {
+                Console.WriteLine("rationalsArray[{0}] = {1}", i, rats[i].ScientificView());
+            }
+            ColoredWriteLine(ConsoleColor.Yellow, "\n" + $"{"Fraction format:\n",64}");
+            for (int i = 0; i < rats.Length; i++)
+            {
+                Console.WriteLine("rationalsArray[{0}] = {1}", i, rats[i].FactionView());
+            }
+            ColoredWriteLine(ConsoleColor.DarkYellow, "\n" + $"{"Conversions from Rationals:\n",70}");
+            Console.WriteLine("(double)rationalsArray[1] = {0}", (double)rats[1]);
+            Console.WriteLine("(string)rationalsArray[0] = {0}", (string)rats[0]);
+        }
+
+        static void ComparisonsDemo(Rationals[] rats)
+        {
+            ColoredWriteLine(ConsoleColor.Red, "\n" + $"{"Demonstration of comparisons:\n",71}");
+            Console.WriteLine("rationalsArray[0] >= 1.5?               {0}", rats[0] >= 1.5);
+            Console.WriteLine("rationalsArray[1] <= rationalsArray[1]? {0}", rats[1] <= rats[1]);
+            Console.WriteLine("rationalsArray[2] > 0?                  {0}", rats[2] > 0);
+            Console.WriteLine("rationalsArray[3] < 12.123?             {0}", rats[3] < 12.123);
+            Console.WriteLine("rationalsArray[4] == 555.5555555?       {0}", rats[4] == 555.5555555);
+            Console.WriteLine("rationalsArray[5] != 17?                {0}", rats[5] != 17);
+            Console.WriteLine("rationalsArray[5].Equals(-999.99999)?   {0}", rats[5].Equals(-999.99999));
+        }
+        static void OperationsDemo(Rationals[] rats)
+        {
+            ColoredWriteLine(ConsoleColor.Cyan, "\n" + $"{"Demonstration of operations:\n",70}");
+            Console.WriteLine($"{rats[0].FactionView()} + 0 = {rats[0] + 0}");
+            Console.WriteLine($"{rats[1]} - 5.6 = {rats[1] - 5.6}");
+            Console.WriteLine($"{rats[2]} * 1.6 = {rats[2] * "1,6"}");
+            Console.WriteLine($"{rats[3]}++ = {++rats[3]}");
+            Console.WriteLine($"{rats[3]}-- = {--rats[3]}");
+            Console.WriteLine($"{rats[5]} / 12 = {rats[5] / 12}");
+            Console.WriteLine($"{rats[4]} /= 5: {rats[4] /= 5}");
+        }
+        static void MistakesDemo(Rationals[] rats)
+        {
+            ColoredWriteLine(ConsoleColor.Blue, "\n" + $"{"Demonstration of possible mistakes:\n",74}");            
+            ExceptionHandler ex1 = () => 
+            {
+                Rationals r = rats[0] / 0;
+            };
+
+            ExceptionHandler ex2 = () =>
+            {
+                Rationals r = new Rationals(1, 0);
+            };
+
+            ExceptionHandler ex3 = delegate()
+            {
+                Rationals r = new Rationals(-1, 2);
+            };
+
+            ExceptionHandler ex4 = delegate ()
+            {
+                Rationals r = "Some wrong string";
+            };
+
+            ExceptionShow(ex1);
+            ExceptionShow(ex2);
+            ExceptionShow(ex3);
+            ExceptionShow(ex4);
+
+            void ExceptionShow(ExceptionHandler ex) 
+            {
+                try
+                {
+                    ex.Invoke();
+                }
+                catch (Exception e) 
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }            
+        }
+    }
+}
---
+++ C#/lab7/Rationals.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Numbers
+{
+    class Rationals : IComparable<Rationals>
+    {
+        private long n;
+        private long m;
+        public long N
+        {
+            get { return n; }
+            set 
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Not natural enumerator");
+                }
+                n = value;
+            }
+        }
+        public long M 
+        {
+            get { return m; }
+            set 
+            {
+                if (value == 0)
+                {
+                    throw new ArgumentException("0 denominator");
+                }
+                m = value;
+            }
+        }
+
+        public Rationals(long n, long m)
+        {
+            N = n;            
+            M = m;
+            Cancel(ref this.n, ref this.m);
+        }
+        public Rationals() 
+        {
+            M = 1;
+            N = 1;            
+        }
+        
+        //conversions 
+        public static implicit operator double(Rationals r)
+        {
+            return (double)r.N / r.M;
+        }
+        public static implicit operator string(Rationals r)
+        {
+            return r.ToString();
+        }
+        public static implicit operator Rationals(double r)
+        {
+            if (r == 0)
+            {
+                return new Rationals(0, 1);
+            }
+            StringBuilder rToString = new StringBuilder(r.ToString());            
+            int powOf10 = 0;
+            int ind;
+            if ((ind = rToString.ToString().IndexOf(',') + 1) != 0)
+            {
+                rToString.Remove(0, ind);
+                powOf10 = rToString.Length;
+            }            
+            if (powOf10 <= 18)
+            {
+                long m = (long)Math.Pow(10, powOf10);
+                long n = (long)(r * m);
+                Cancel(ref n, ref m);
+                return new Rationals(n, m);
+            }
+            else
+            {
+                throw new ArgumentException("Exceeded maximum length of number");
+            }                
+        }
+        public static implicit operator Rationals(string s)
+        {
+            //checking with regular expression pattern
+            string pattern = @"^\d{1,18}[\.|\,]?\d{0,18}";
+            if (Regex.IsMatch(s, pattern))
+            {
+                StringBuilder result = new StringBuilder(s);
+                if (s.Contains("."))
+                {
+                    result = result.Replace('.', ',');
+                }
+                return double.Parse(result.ToString());
+            }
+            else
+            {
+                throw new ArgumentException("Wrong string format for conversion to Rationals");
+            }
+        }
+
+        //operators overloading
+        public static Rationals operator +(Rationals r1, Rationals r2) 
+        {
+            long n = r1.N * r2.M + r2.N * r1.M;
+            long m = r1.M * r2.M;
+            Sign(ref n, ref m);
+            return new Rationals(n, m);
+        }
+        public static Rationals operator -(Rationals r1, Rationals r2)
+        {
+            long n = r1.N * r2.M - r2.N * r1.M;
+            long m = r1.M * r2.M;
+            Sign(ref n, ref m);
+            return new Rationals(n, m);
+        }
+        public static Rationals operator *(Rationals r1, Rationals r2)
+        {
+            long n = r1.N * r2.N;
+            long m = r1.M * r2.M;
+            Sign(ref n, ref m);
+            return new Rationals(n, m);
+        }
+        public static Rationals operator /(Rationals r1, Rationals r2)
+        {
+            long n = r1.N * r2.M;
+            long m = r1.M * r2.N;
+            Sign(ref n, ref m);
+            return new Rationals(n, m);
+        }
+        public static Rationals operator ++(Rationals r1)
+        {
+            long n = r1.N + r1.M;
+            long m = r1.M;
+            Sign(ref n, ref m);
+            return new Rationals(n, m);
+        }
+        public static Rationals operator --(Rationals r1)
+        {
+            long n = r1.N - r1.M;
+            long m = r1.M;
+            Sign(ref n, ref m);
+            return new Rationals(n, m);            
+        }
+
+        //method to ensure Enumerator is Natural
+        private static void Sign(ref long n, ref long m) 
+        {
+            if (n < 0)
+            {
+                n = Math.Abs(n);
+                m *= -1;
+            }
+        }
+        //method to reduce fraction
+        private static void Cancel(ref long n, ref long m) 
+        {
+            int maxSieve;
+            long absN = Math.Abs(n);
+            long absM = Math.Abs(m);
+            if (absN > absM)
+            {                
+                maxSieve = (int)Math.Sqrt(absN);
+            }
+            else
+            {
+                maxSieve = (int)Math.Sqrt(absM);
+            }
+            for (int i = 2; i < maxSieve; i++)
+            {
+                if (n % i == 0 && m % i == 0)
+                {
+                    n /= i;
+                    m /= i;
+                }
+            }
+            Sign(ref n, ref m);
+        }
+
+        //different string format
+        public string DoubleView() 
+        {
+            return ToString();
+        }
+        public string FactionView() 
+        {
+            return $"{N}/{M}";
+        }
+        public string ScientificView() 
+        {
+            return $"{(double)N/M:E}";
+        }
+
+        //comparisons + System.Object mehods override
+        public override string ToString()
+        {
+            return ((double)N / M).ToString();
+        }
+        public override int GetHashCode()
+        {
+            return ToString().GetHashCode();
+        }
+        public override bool Equals(object obj)
+        {
+            return ToString() == obj.ToString();
+        }
+        public static bool operator ==(Rationals a, Rationals b) => a.Equals(b);
+        public static bool operator !=(Rationals a, Rationals b) => !(a.Equals(b));
+
+        int IComparable<Rationals>.CompareTo(Rationals other)
+        {
+            if ((double)N / M > (double)other.N / other.M)
+            {
+                return 1;
+            }
+            if ((double)N / M < (double)other.N / other.M)
+            {
+                return -1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        public static bool operator >(Rationals a, Rationals b) => ((IComparable<Rationals>)a).CompareTo(b) == 1;
+        public static bool operator <(Rationals a, Rationals b) => ((IComparable<Rationals>)a).CompareTo(b) == -1;
+        public static bool operator >=(Rationals a, Rationals b) => ((IComparable<Rationals>)a).CompareTo(b) >= 0;
+        public static bool operator <=(Rationals a, Rationals b) => ((IComparable<Rationals>)a).CompareTo(b) <= 0;
+    }
+}
